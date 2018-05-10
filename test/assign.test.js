9e396d708e2a99e05f373796c8c44bc82bb7fcef
luojinghui <luojinghui@xylink.com>
2018-05-10 20:50:58 +0800

    init project

--- test/assign.test.js
+++ test/assign.test.js
@@ -11,20 +11,34 @@ describe("Object.assign", () => {
     c: 89
   };
 
+  const errorTarget = [1,2,4];
+
   const expected = {
     a: "3",
     b: 3,
     c: 89
   };
 
-  test("should return a object", () => {
+  const notObject = "Not a object";
 
+  test("should return a object", () => {
+    let result = assign(target,source);
+    let expectResult = false;
+
+    if(Object.prototype.toString.call(result) === "[object Object]") {
+      expectResult = true;
+    }
+
+    expect(expectResult).toBe(true);
   });
 
   test("throw when target is not a object", () => {
+    expect(assign(errorTarget, source)).toBe(notObject);
   });
 
   test("should return a object equal expected", () => {
+    let result = assign(target, source);
 
+    expect(result).toEqual(expected);
   });
 });
